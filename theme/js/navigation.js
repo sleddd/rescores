bfabd12e51fd96465af2225adba4ba5e71efcd2d
Claudette raynor <claudettemraynor@gmail.com>
2023-02-16 18:25:34 -0800

    Fully working menu - not mobile tested, no aria

--- theme/js/navigation.js
+++ theme/js/navigation.js
@@ -6,17 +6,53 @@
 (function ($) {
 
 	// Dropdown trigger events.
-	$( '.menu-item-has-children a').on('click', function( e ) { return openDropdownMenu( e ) });
-	$( '.menu-item-has-children').hover( function(e) { $( this ).find('a').trigger('click'); });
+	$( '.menu-item-has-children a').on('click', function( e ) { return toggleDropdownMenu( e ) });
+
+	// Dropdown on hover.
+	$( '.menu-item-has-children').on( 'mouseenter', function( e ) {
+		var self = this;
+		setTimeout( function() {
+			if ( ! $('.navbar').hasClass('navbar--open') ) {
+				$( self ).find('a').first().trigger('click');
+			}
+		}, 100 );
+	});
+
+	// Dropdown on hover end.
+	$( '.menu-item-has-children').on( 'mouseleave', function( e ) {
+		var self = this; 
+		setTimeout( function() {
+			if ( ! $('.navbar').hasClass('navbar--open') ) {
+				$( self ).find('a').first().trigger('click');
+			}
+		}, 100 );
+	});
+		
+		
 
 	// Handles dropdown functionality for menu items.
-    function openDropdownMenu(e) {
+    function toggleDropdownMenu(e) {
 		//TODO: Add aria changes.
-		$( e.target ).parent().find('.sub-menu').first().toggleClass('sub-menu--open');
-		if (
-			e.detail === 1 &&
-			$( e.target ).parent().hasClass('menu-item-has-children')
-		) {
+		if ( $( e.target ).parent().find('.sub-menu').first().hasClass('sub-menu--open') ) {
+			// Close child menus.
+			$( e.target ).parent().find('.sub-menu').first().find('.sub-menu').removeClass('sub-menu--open');
+			// Close parent.
+			$( e.target ).parent().find('.sub-menu').first().removeClass('sub-menu--open');
+			// Remove semibold for mobile menu.
+			if ( $('header .navbar').hasClass('navbar--open') ) {
+				$( e.target ).removeClass('semibold');
+			}
+		} else {
+			$( e.target ).parent().find('.sub-menu').first().addClass('sub-menu--open');
+			// Add semibold for mobile menu.
+			if ( $('header .navbar').hasClass('navbar--open') ) {
+				$( e.target ).addClass('semibold');
+			}
+		}
+
+		// Ensures that the top anchor shows drodown first, then if double clicked,
+		// it continues on to the link url.
+		if ( e.detail === 1 && $( e.target ).parent().hasClass('menu-item-has-children') ) {
 			// Single click - stop event.
 			e.preventDefault();
 		}
