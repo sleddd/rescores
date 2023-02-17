e388ae10e7ae5b3655495edc6f93011864a57296
Claudette raynor <claudettemraynor@gmail.com>
2023-02-16 21:42:22 -0800

    Added final menu with styles and aria controls - keyboard, not mobile tested

--- theme/js/navigation.js
+++ theme/js/navigation.js
@@ -5,6 +5,15 @@
  */
 (function ($) {
 
+	$( '.menu-item-has-children a').each( function( index ) {
+		$( this ).attr('role', 'button');
+		$( this ).attr('aria-haspopup', 'listbox');
+		$( this ).attr('aria-expanded', 'false');
+		$( this ).attr('aria-labelledby', `menu-listbox-${index.toString()}` );
+		$( this ).attr('aria-label', `Menu Listbox Dropdown ${index.toString()}` );
+		$( this ).attr('id', `menu-listbox-${index.toString()}` );
+	});
+
 	// Dropdown trigger events.
 	$( '.menu-item-has-children a').on('click', function( e ) { return toggleDropdownMenu( e ) });
 
@@ -27,40 +36,55 @@
 			}
 		}, 100 );
 	});
-		
-		
 
 	// Handles dropdown functionality for menu items.
-    function toggleDropdownMenu(e) {
-		//TODO: Add aria changes.
+    function toggleDropdownMenu( e ) {
+
+		// Ensures that the top anchor shows drodown first, then if double clicked,
+		// it continues on to the link url.
+		// if ( e.detail === 1 && $( e.target ).parent().hasClass('menu-item-has-children') ) {
+		// 	// Single click - stop event.
+		// 	e.preventDefault();
+		// }
+		e.preventDefault();
+
 		if ( $( e.target ).parent().find('.sub-menu').first().hasClass('sub-menu--open') ) {
 			// Close child menus.
 			$( e.target ).parent().find('.sub-menu').first().find('.sub-menu').removeClass('sub-menu--open');
+			$( e.target ).parent().find('.sub-menu').first().find('.sub-menu').attr('aria-expanded', 'false');
 			// Close parent.
 			$( e.target ).parent().find('.sub-menu').first().removeClass('sub-menu--open');
+			$( e.target ).attr('aria-expanded', 'false');
+
 			// Remove semibold for mobile menu.
 			if ( $('header .navbar').hasClass('navbar--open') ) {
 				$( e.target ).removeClass('semibold');
 			}
 		} else {
 			$( e.target ).parent().find('.sub-menu').first().addClass('sub-menu--open');
+			$( e.target ).attr('aria-expanded', 'true');
+
 			// Add semibold for mobile menu.
 			if ( $('header .navbar').hasClass('navbar--open') ) {
+				$('header .navbar').find('a').removeClass('semibold');
 				$( e.target ).addClass('semibold');
 			}
-		}
-
-		// Ensures that the top anchor shows drodown first, then if double clicked,
-		// it continues on to the link url.
-		if ( e.detail === 1 && $( e.target ).parent().hasClass('menu-item-has-children') ) {
-			// Single click - stop event.
-			e.preventDefault();
 		}
 	}
 
 	// Mobile Menu toggle event.
-	$('.menu-toggle').on('click', function (e) {
-		$('header .navbar').toggleClass('navbar--open');
-	});
+	$('.menu-toggle').on('click', function (e) { toggleNavbar(e); });
+
+	function toggleNavbar( e ) {
+		if ( $('header .navbar').hasClass('navbar--open') ) {
+			$('header .navbar').removeClass('navbar--open');
+			$( this ).attr('aria-expanded', 'false');
+			$('header .navbar').find('a').removeClass('semibold');
+			$('.sub-menu').removeClass('sub-menu--open');
+		} else {
+			$('header .navbar').addClass('navbar--open');
+			$( this ).attr('aria-expanded', 'true');
+		}
+	}
 
 })( jQuery );
--- theme/patterns/header.php
+++ theme/patterns/header.php
@@ -5,10 +5,9 @@
  * Categories: header
  */
 ?>
-<!-- wp:group {"align":"full"},"tagName", "header", "className":"header shadow-lg bg-gray-50","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} -->
+<!-- wp:group {"className":"alignfull header shadow-lg bg-gray-50", "tagName": "header"} -->
 <header class="wp-block-group alignfull header shadow-lg bg-gray-50"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"bottom":"0","top":"0"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
-<div class="wp-block-group alignfull" style="padding-top:0;padding-bottom:0">
-<!-- wp:group {"layout":{"type":"flex"}, "className":"site-info"} -->
+<div class="wp-block-group alignfull" style="padding-top:0;padding-bottom:0"><!-- wp:group {"className":"site-info","layout":{"type":"flex"}} -->
 <div class="wp-block-group site-info"><!-- wp:site-logo {"width":60} /-->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}}} -->
@@ -18,22 +17,23 @@
 <!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
+
 <!-- wp:html -->
-	<nav class="navbar">
-        <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
-            <div class="sr-only"><?php esc_html_e( 'Primary Menu Button', '_s' ); ?></div>
-            <div class="bar"></div>
-            <div class="bar"></div>
-            <div class="bar"></div>
-        </button>
-        <?php
-        wp_nav_menu(
-            array(
-                'theme_location' => 'menu-1',
-                'menu_id'        => 'primary-menu',
-            )
-        );
-        ?>
-	</nav>
-<!-- /wp:html -->
-</header><!-- /wp:group -->
+<nav class="navbar" id="navbar">
+    <button class="menu-toggle" aria-controls="navbar" aria-expanded="false" aria-label="Toggle open mobile navbar menu items">
+        <div class="sr-only"><?php esc_html_e( 'Mobile Menu Toggle Button', '_rs' ); ?></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+    </button>
+    <?php
+    wp_nav_menu(
+        array(
+            'theme_location' => 'menu-1',
+            'menu_id'        => 'primary-menu',
+        )
+    );
+    ?>
+</nav>
+<!-- /wp:html --></header>
+<!-- /wp:group -->
