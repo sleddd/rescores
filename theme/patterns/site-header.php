1f4b88d81fbf7628ed3fbeafd40cedd4d31f7596
Claudette raynor <claudettemraynor@gmail.com>
2023-02-28 20:12:52 -0800

    Scaled fonts for responsive,  updated screenshot, header, and page layout one

--- theme/patterns/site-header.php
+++ theme/patterns/site-header.php
@@ -5,8 +5,8 @@
  * Categories: header
  */
 ?>
-<!-- wp:group {"tagName":"header","style":{"spacing":{"padding":{"top":"1rem","right":"1rem","bottom":"1rem","left":"1rem"}}},"className":"alignfull header shadow-lg bg-gray-50"} -->
-	<header class="wp-block-group alignfull header shadow-lg bg-gray-50" style="padding-top:1rem;padding-right:1rem;padding-bottom:1rem;padding-left:1rem">
+<!-- wp:group {"tagName":"header","style":{"spacing":{"padding":{"top":"1.5rem","right":"1rem","bottom":"1.7rem","left":"1rem"}}},"className":"alignfull header shadow-lg bg-gray-50"} -->
+	<header class="wp-block-group alignfull header shadow-lg bg-gray-50" style="padding-top:1.5rem;padding-right:1rem;padding-bottom:1.7rem;padding-left:1rem">
 		<!-- wp:group {"style":{"spacing":{"margin":{"top":"0em","right":"1em","bottom":"0em","left":"0em"}}},"className":"site-info","layout":{"type":"flex"}} -->
 			<div class="wp-block-group site-info" style="margin-top:0em;margin-right:1em;margin-bottom:0em;margin-left:0em">
 				<!-- wp:template-part {"slug":"logo","theme":"rescores"} /-->
