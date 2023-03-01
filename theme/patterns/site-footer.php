8c7c92c42f823b071411ebc99393d6f04839f3ff
Claudette raynor <claudettemraynor@gmail.com>
2023-02-28 18:26:00 -0800

    Stuck footer to bottom of page, adjusted page layout one text, fixed woocommerce info notice overlap

--- theme/patterns/site-footer.php
+++ theme/patterns/site-footer.php
@@ -5,8 +5,8 @@
  * Categories: footer
  */
 ?>
-<!-- wp:group {"style":{"color":{"background":"#f1f5f9"},"border":{"top":{"color":"var:preset|color|gray-md","width":"1px"}}},"className":"has-background","layout":{"type":"constrained","tagName":"footer","className":"footer"}} -->
-	<div class="wp-block-group has-background" style="border-top-color:#e2e8f0;border-top-width:1px;background-color:#f1f5f9">
+<!-- wp:group {"style":{"color":{"background":"#f1f5f9"},"border":{"top":{"color":"var:preset|color|gray-md","width":"1px"}}},"className":"footer has-background","layout":{"type":"constrained","tagName":"footer","className":"footer"}} -->
+	<div class="wp-block-group footer has-background" style="border-top-color:#e2e8f0;border-top-width:1px;background-color:#f1f5f9">
 		<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|10","left":"var:preset|spacing|10"}}},"className":"alignfull","layout":{"type":"flex","justifyContent":"space-between"}} -->
 			<div class="wp-block-group alignfull" style="padding-right:var(--wp--preset--spacing--10);padding-left:var(--wp--preset--spacing--10)">
 				<!-- wp:site-title {"level":0,"style":{"elements":{"link":{"color":{"text":"var:preset|color|link-active"}}}},"fontSize":"medium"} /-->
